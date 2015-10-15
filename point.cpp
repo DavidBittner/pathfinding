49d15428e6f029c8f3db36a9f2de93294bca0460
David <bittneradave@gmail.com>
2015-10-14 22:20:55 -0400

    More work, added heapsort

--- point.cpp
+++ point.cpp
@@ -35,3 +35,20 @@ Point::Point( int x, int y, int cost )
 	_parent = nullptr;
 
 }
+
+bool Point::operator>( Point const b ) const
+{
+
+	return( this->_cost > b._cost );
+
+}
+
+int Point::manhattanDistance( Coord pos )
+{
+
+	int x = abs( pos.x - _pos.x );
+	int y = abs( pos.y - _pos.y );
+
+	return x+y;
+
+}
