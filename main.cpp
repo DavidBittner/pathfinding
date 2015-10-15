f97520c4f4a86f183878ff79c94e32c3396ece68
david <bittneradave@gmail.com>
2015-10-15 14:26:52 -0400

    Ahh! It loops forever!

--- main.cpp
+++ main.cpp
@@ -19,10 +19,20 @@ void Reshape( GLFWwindow *wind, int width, int height )
 
 }
 
+int manhattan( Coord a, Coord b )
+{
+
+	int x = abs(b.x-a.x);
+	int y = abs(b.y-a.y);
+
+	return x+y;
+
+}
+
 void AStar( std::vector< std::vector< bool > > grid, Point start, Point end )
 {
 
-	Point *cur = &start;
+	Point *cur;
 
 	std::vector< Point* > closed;
 	std::vector< Point* > open;
@@ -32,6 +42,8 @@ void AStar( std::vector< std::vector< bool > > grid, Point start, Point end )
 	while( open.size() > 0 )
 	{
 
+		cur = open.at(0);
+
 		if( cur->getPos() == end.getPos() )
 		{
 
@@ -40,12 +52,72 @@ void AStar( std::vector< std::vector< bool > > grid, Point start, Point end )
 
 		}
 
+		for( int y = -1; y <= 1; y++ )
+		{
+
+			for( int x = -1; x <= 1; x++ )
+			{
+
+				int curX = cur->getPos().x+x;
+				int curY = cur->getPos().y+y;
+
+				Coord temp( curX, curY );
+
+				bool make = true;
+
+				for( int i = 0; i < open.size(); i++ )
+				{
+
+					if( temp == open.at(i)->getPos() )
+					{
+
+						make = false;
+
+					}
+
+				}
+
+				for( int i = 0; i < closed.size(); i++ )
+				{
+
+					if( temp == closed.at(i)->getPos() )
+					{
+
+						make = false;
+
+					}
+
+				}
+
+				if( make )
+				{
+		
+					open.push_back( new Point( curX, curY, manhattan( cur->getPos(), temp ), cur ) );
+
+				}
+		
+			}
+
+		}
+
+		closed.push_back( open.at(0) );
+		open.erase( open.begin() );
+
+		open = heapsort( open );
+
 	}
 
 }
 
 int main()
 {
+
+	std::vector< std::vector< bool > > a;
+
+	Point start( 1, 1, 0, nullptr );
+	Point end( 5, 1, 0, nullptr );
+
+	AStar( a, start, end );
 
 	glfwInit();
 	GLFWwindow *wind = glfwCreateWindow( 800, 600, "A* Pathfinding", nullptr, nullptr );
